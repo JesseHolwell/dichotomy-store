63fa5bbfae37e9fc46ba1b2330698b7d892b6781
Jesse Holwell <jesse.d.holwell@gmail.com>
2024-07-15 21:06:48 -0500

    display placeholder homepage

--- card-deck-app/src/App.js
+++ card-deck-app/src/App.js
@@ -5,18 +5,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h1 className="App-heading">Dichotomy cards</h1>
+        <p className="App-text">
+          Site under construction
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
