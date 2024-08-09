c4a1a07fae9906b858d8a4acc297175587856b98
Jesse Holwell <jesse.d.holwell@gmail.com>
2024-08-09 17:27:10 -0500

    fix navbar in mobile view

--- card-deck-app/src/components/Navbar.tsx
+++ card-deck-app/src/components/Navbar.tsx
@@ -67,19 +67,19 @@ const NavLink = (props: Props) => {
 };
 
 export default function Simple() {
-  // const { isOpen, onOpen, onClose } = useDisclosure()
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <>
       <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
         <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
-          {/* <IconButton
-            size={'md'}
+          <IconButton
+            size={"md"}
             icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-            aria-label={'Open Menu'}
-            display={{ md: 'none' }}
+            aria-label={"Open Menu"}
+            display={{ md: "none" }}
             onClick={isOpen ? onClose : onOpen}
-          /> */}
+          />
           <HStack spacing={8} alignItems={"center"} h={"100%"}>
             {/* <Box boxSize="sm"> */}
             <Image
@@ -104,6 +104,15 @@ export default function Simple() {
             </HStack>
           </HStack>
         </Flex>
+        {isOpen ? (
+          <Box pb={4} display={{ md: "none" }}>
+            <Stack as={"nav"} spacing={4}>
+              {Links.map((link) => (
+                <NavLink key={link.value}>{link.label}</NavLink>
+              ))}
+            </Stack>
+          </Box>
+        ) : null}
       </Box>
 
       {/* <Box p={4}>Main Content Here</Box> */}
