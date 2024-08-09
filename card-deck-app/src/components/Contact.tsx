656c1c1563333653700100907dbd0eac29afd72e
Jesse Holwell <jesse.d.holwell@gmail.com>
2024-08-09 16:43:42 -0500

    build contact page

--- card-deck-app/src/components/Contact.tsx
+++ card-deck-app/src/components/Contact.tsx
@@ -1,83 +1,256 @@
-import React, { useState } from "react";
-import emailjs from "emailjs-com";
+// import React, { useState } from "react";
+// import emailjs from "emailjs-com";
 
-function Contact() {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
+// function Contact() {
+//   const [formData, setFormData] = useState({
+//     name: "",
+//     email: "",
+//     message: "",
+//   });
 
-  const handleChange = (e: any) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
+//   const handleChange = (e: any) => {
+//     const { name, value } = e.target;
+//     setFormData({
+//       ...formData,
+//       [name]: value,
+//     });
+//   };
 
-  const handleSubmit = (e: any) => {
-    e.preventDefault();
+//   const handleSubmit = (e: any) => {
+//     e.preventDefault();
 
-    emailjs
-      .send(
-        "service_ynfc32s",
-        "template_sx4gpsm",
-        {
-          user_name: formData.name,
-          user_email: formData.email,
-          message: formData.message,
-        },
-        process.env.REACT_APP_EMAILJS_PUBLICKEY
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-          alert("Message sent successfully");
-        },
-        (error) => {
-          console.log(error.text);
-          alert("Failed to send message");
-        }
-      );
+//     emailjs
+//       .send(
+//         "service_ynfc32s",
+//         "template_sx4gpsm",
+//         {
+//           user_name: formData.name,
+//           user_email: formData.email,
+//           message: formData.message,
+//         },
+//         process.env.REACT_APP_EMAILJS_PUBLICKEY
+//       )
+//       .then(
+//         (result) => {
+//           console.log(result.text);
+//           alert("Message sent successfully");
+//         },
+//         (error) => {
+//           console.log(error.text);
+//           alert("Failed to send message");
+//         }
+//       );
 
-    setFormData({ name: "", email: "", message: "" });
-  };
+//     setFormData({ name: "", email: "", message: "" });
+//   };
 
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <div>
+//         <label>Name</label>
+//         <input
+//           type="text"
+//           name="name"
+//           value={formData.name}
+//           onChange={handleChange}
+//           required
+//         />
+//       </div>
+//       <div>
+//         <label>Email</label>
+//         <input
+//           type="email"
+//           name="email"
+//           value={formData.email}
+//           onChange={handleChange}
+//           required
+//         />
+//       </div>
+//       <div>
+//         <label>Message</label>
+//         <textarea
+//           name="message"
+//           value={formData.message}
+//           onChange={handleChange}
+//           required
+//         />
+//       </div>
+//       <button type="submit">Send</button>
+//     </form>
+//   );
+// }
+
+// export default Contact;
+
+"use client";
+
+import {
+  Container,
+  Flex,
+  Box,
+  Heading,
+  Text,
+  IconButton,
+  Button,
+  VStack,
+  HStack,
+  Wrap,
+  WrapItem,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Textarea,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import {
+  MdPhone,
+  MdEmail,
+  MdLocationOn,
+  MdFacebook,
+  MdOutlineEmail,
+} from "react-icons/md";
+import { BsGithub, BsDiscord, BsPerson } from "react-icons/bs";
+
+export default function Contact() {
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>Name</label>
-        <input
-          type="text"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div>
-        <label>Email</label>
-        <input
-          type="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div>
-        <label>Message</label>
-        <textarea
-          name="message"
-          value={formData.message}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <button type="submit">Send</button>
-    </form>
+    <Container maxW="full" mt={0} centerContent overflow="hidden">
+      <Flex>
+        <Box
+          bg={useColorModeValue("gray.100", "gray.900")}
+          borderRadius="lg"
+          m={{ sm: 4, md: 16, lg: 10 }}
+          p={{ sm: 5, md: 5, lg: 16 }}
+        >
+          <Box p={4}>
+            <Wrap spacing={{ base: 20, sm: 3, md: 5, lg: 20 }}>
+              <WrapItem>
+                <Box>
+                  <Heading>Contact</Heading>
+                  <Text mt={{ sm: 3, md: 3, lg: 5 }} color="gray.500">
+                    Fill up the form below to contact
+                  </Text>
+                  <Box py={{ base: 5, sm: 5, md: 8, lg: 10 }}>
+                    <VStack pl={0} spacing={3} alignItems="flex-start">
+                      <Button
+                        size="md"
+                        height="48px"
+                        width="200px"
+                        variant="ghost"
+                        color="#cc7700"
+                        _hover={{ border: "2px solid #cc7700" }}
+                        leftIcon={<MdPhone color="#ff7700" size="20px" />}
+                      >
+                        +61 555 555 555
+                      </Button>
+                      <Button
+                        size="md"
+                        height="48px"
+                        width="200px"
+                        variant="ghost"
+                        color="#cc7700"
+                        _hover={{ border: "2px solid #cc7700" }}
+                        leftIcon={<MdEmail color="#ff7700" size="20px" />}
+                      >
+                        support@dichotomycards.com
+                      </Button>
+                      <Button
+                        size="md"
+                        height="48px"
+                        width="200px"
+                        variant="ghost"
+                        color="#cc7700"
+                        _hover={{ border: "2px solid #cc7700" }}
+                        leftIcon={<MdLocationOn color="#ff7700" size="20px" />}
+                      >
+                        Melbourne, Australia
+                      </Button>
+                    </VStack>
+                  </Box>
+                  {/* <HStack
+                    mt={{ lg: 10, md: 10 }}
+                    spacing={5}
+                    px={5}
+                    alignItems="flex-start"
+                  >
+                    <IconButton
+                      aria-label="facebook"
+                      variant="ghost"
+                      size="lg"
+                      isRound={true}
+                      _hover={{ bg: "#0D74FF" }}
+                      icon={<MdFacebook size="28px" />}
+                    />
+                    <IconButton
+                      aria-label="github"
+                      variant="ghost"
+                      size="lg"
+                      isRound={true}
+                      _hover={{ bg: "#0D74FF" }}
+                      icon={<BsGithub size="28px" />}
+                    />
+                    <IconButton
+                      aria-label="discord"
+                      variant="ghost"
+                      size="lg"
+                      isRound={true}
+                      _hover={{ bg: "#0D74FF" }}
+                      icon={<BsDiscord size="28px" />}
+                    />
+                  </HStack> */}
+                </Box>
+              </WrapItem>
+              <WrapItem>
+                <Box bg="white" borderRadius="lg">
+                  <Box m={8} color="#0B0E3F">
+                    <VStack spacing={5}>
+                      <FormControl id="name">
+                        <FormLabel>Your Name</FormLabel>
+                        <InputGroup borderColor="#E0E1E7">
+                          <InputLeftElement pointerEvents="none">
+                            <BsPerson color="gray.800" />
+                          </InputLeftElement>
+                          <Input type="text" size="md" />
+                        </InputGroup>
+                      </FormControl>
+                      <FormControl id="name">
+                        <FormLabel>Email</FormLabel>
+                        <InputGroup borderColor="#E0E1E7">
+                          <InputLeftElement pointerEvents="none">
+                            <MdOutlineEmail color="gray.800" />
+                          </InputLeftElement>
+                          <Input type="text" size="md" />
+                        </InputGroup>
+                      </FormControl>
+                      <FormControl id="name">
+                        <FormLabel>Message</FormLabel>
+                        <Textarea
+                          borderColor="gray.300"
+                          _hover={{
+                            borderRadius: "gray.300",
+                          }}
+                          placeholder="Tell us how we can help"
+                        />
+                      </FormControl>
+                      <FormControl id="name" float="right">
+                        <Button
+                          variant="solid"
+                          bg="#ff7700"
+                          color="white"
+                          _hover={{}}
+                        >
+                          Send Message
+                        </Button>
+                      </FormControl>
+                    </VStack>
+                  </Box>
+                </Box>
+              </WrapItem>
+            </Wrap>
+          </Box>
+        </Box>
+      </Flex>
+    </Container>
   );
 }
-
-export default Contact;
