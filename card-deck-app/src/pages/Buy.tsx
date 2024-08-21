c2a2ca766dd008eec7bf9cb71f321ee541e8132e
Jesse Holwell <jesse.d.holwell@gmail.com>
2024-08-20 23:22:15 -0500

    quick and dirty checkout form to test purchases

--- card-deck-app/src/pages/Buy.tsx
+++ card-deck-app/src/pages/Buy.tsx
@@ -1,38 +1,104 @@
-import { Button, Container, Flex, Stack } from "@chakra-ui/react";
+import { Box, Button, Container, Stack } from "@chakra-ui/react";
 import axios from "axios";
 import React from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import {
+  Elements,
+  CardElement,
+  useStripe,
+  useElements,
+} from "@stripe/react-stripe-js";
 
-function Buy() {
-  const handleClick = async () => {
+const stripePromise = loadStripe(
+  process.env.REACT_APP_STRIPE_PUBLIC_KEY as string
+);
+
+function CheckoutForm() {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleClick = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      // Disable form submission until Stripe.js has loaded
+      return;
+    }
+
     try {
-      // Access the environment variable
       const apiUrl = process.env.REACT_APP_API_URL;
       const response = await axios.post(
-        `${apiUrl}/v1/purchase/createPurchase`,
+        `${apiUrl}/v1/purchase/createPaymentIntent`,
         {
-          // key: "value", // Replace with your data
+          // TODO: real values
+          amount: 999,
+          currency: "AUD",
         }
       );
-      console.log("Response:", response.data);
+
+      const { clientSecret } = response.data;
+
+      if (!clientSecret) {
+        throw new Error("Client secret is missing from the response.");
+      }
+
+      const result = await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: elements.getElement(CardElement)!,
+        },
+      });
+
+      if (result.error) {
+        console.error("Payment failed", result.error);
+      } else if (result.paymentIntent?.status === "succeeded") {
+        console.log("Payment succeeded");
+
+        const response = await axios.post(
+          `${apiUrl}/v1/purchase/savePurchase`,
+          {
+            stripeTransactionId: result.paymentIntent.id,
+            amount: result.paymentIntent.amount,
+            name: "Web Test",
+            email: "web@test.com",
+            phone: "0404404404",
+            shippingAddress: "the world wide web",
+          }
+        );
+      }
     } catch (error) {
       console.error("Error:", error);
     }
   };
 
   return (
-    <Container as={Stack} maxW={"6xl"} py={56} mt={56} align={"center"}>
+    <form onSubmit={handleClick}>
+      <Box>
+        <CardElement />
+      </Box>
       <Button
         rounded={"full"}
         size={"lg"}
         fontWeight={"normal"}
         px={6}
+        mx={32}
+        my={32}
         colorScheme={"orange"}
         bg={"orange.400"}
         _hover={{ bg: "orange.500" }}
-        onClick={handleClick}
+        type="submit"
       >
-        Buy the dichotomy card game
+        Buy the Dichotomy card game
       </Button>
+    </form>
+  );
+}
+
+function Buy() {
+  return (
+    <Container as={Stack} maxW={"6xl"} w="500" py={56} mt={56} align={"center"}>
+      <Elements stripe={stripePromise}>
+        <CheckoutForm />
+      </Elements>
     </Container>
   );
 }
